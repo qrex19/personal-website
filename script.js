dd963069f5791c740811c5467e891a48dfe160ad
qrex19 <nishanttjhaa25@gmail.com>
2023-06-27 20:18:46 +0530

    project completed

---
+++ script.js
@@ -0,0 +1,18 @@
+
+//making icons clickable
+
+
+const github = document.querySelector('#github-icon');
+const twitter = document.querySelector('#twitter-icon');
+
+github.addEventListener('click', function run() {
+
+    open('https://github.com/qrex19');
+
+});
+
+twitter.addEventListener('click', function run() {
+
+    open('https://twitter.com/nishants_twt');
+
+});

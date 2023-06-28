c755f86f3843adb9461633051597f8b9fa205b54
qrex19 <nishanttjhaa25@gmail.com>
2023-06-28 21:16:39 +0530

    formatted using prettier

--- script.js
+++ script.js
@@ -1,18 +1,12 @@
-
 //making icons clickable
 
+const github = document.querySelector("#github-icon");
+const twitter = document.querySelector("#twitter-icon");
 
-const github = document.querySelector('#github-icon');
-const twitter = document.querySelector('#twitter-icon');
-
-github.addEventListener('click', function run() {
-
-    open('https://github.com/qrex19');
-
+github.addEventListener("click", function run() {
+  open("https://github.com/qrex19");
 });
 
-twitter.addEventListener('click', function run() {
-
-    open('https://twitter.com/nishants_twt');
-
+twitter.addEventListener("click", function run() {
+  open("https://twitter.com/nishants_twt");
 });
